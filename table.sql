c2881641e08faaa77f1d12154302c34f04220e64
Pratik <pmgajjar25@gmail.com>
2025-07-06 19:44:12 +0530

    Add table

---
+++ table.sql
@@ -0,0 +1,45 @@
+create table edge (
+    source UInt64,
+    target UInt64,
+    weight Float64,
+) ENGINE = MergeTree()
+PRIMARY KEY (source, target)
+ORDER BY (source ASC, target ASC);
+
+create table vertex (
+    id UInt64,
+    label String,
+    INDEX vertex_label_text_idx(label) TYPE text(tokenizer = 'default'),
+    PRIMARY KEY (id)
+) ENGINE = ReplacingMergeTree()
+ORDER BY id;
+
+ALTER TABLE vertex (MATERIALIZE INDEX vertex_label_text_idx);
+
+-- Example data
+INSERT INTO edge (source, target, weight)
+VALUES (1, 2, 1.0),
+       (1, 3, 2.0),
+       (1, 4, 2.0),
+       (2, 3, 2.0),
+       (2, 3, 2.0),
+       (3, 1, 3.0);
+
+INSERT INTO vertex (id, label)
+VALUES (1, 'pratik gajjar'),
+       (2, 'vaibhav patel'),
+       (3, 'dhruv patel'),
+       (4, 'dhruv prajapati'),
+       (5, 'daval dhingra'),
+       (6, 'dhara patel');
+
+INSERT INTO vertex(id, label) SELECT id, title from hackernews where length(title) > 3 and length(title) < 40;
+-- INSERT 1000 vertex
+select count(*) from hackernews where length(title) > 3 and length(title) < 40 limit 5;
+
+-- Find match which starts with edge
+
+
+SELECT * FROM vertex WHERE match(label, '^dh| dh') AND id in (SELECT target FROM edge WHERE source = 1);
+
+EXPLAIN indexes = 1 SELECT * FROM vertex WHERE label like  '%test%';
